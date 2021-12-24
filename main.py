00a1b559cfb80e63d634f488311cfd517754e3e4
CodingSoda <26rkshah@gmail.com>
2021-12-23 21:25:04 -0800

    more work

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def rm_cmnts():
-    lines = open("input.txt", "r").read().splitlines()
+def init_files() -> list:
+    try:
+        print("Looks like you don't have your files set up, I did it for you.")
+        return open("input.txt", "x").read().splitlines()
+    except FileExistsError:
+        return open("input.txt", "r").read().splitlines()
+
+
+def rm_cmnts(lines: list) -> int:
     lines = [line.strip() for line in lines]
 
     with open("output.txt", "w") as out:
