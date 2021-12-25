67f7f0acff359bb6e2eaed291b6fa64048b0657c
CodingSoda <26rkshah@gmail.com>
2021-12-24 20:53:49 -0800

    Made 'padding_len()' in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def init_files(input_name: str = "input.txt", output_name: str = "output.txt") -> list:
+    """Initializes the files if not already."""
 
     # Input
     try:
@@ -21,6 +22,7 @@ lines = init_files("input.txt", "output.txt")
 
 
 def rm_cmnts(lines: list, output_name: str = "output.txt") -> int:
+    """Removes the comments in the input file, outputs in output file."""
     lines = [line.strip() for line in lines]
     err = 0
 
@@ -38,14 +40,29 @@ print(f"Removed Comments with {rm_cmnts(lines, 'output.txt')}")
 
 
 def split_filelines(filename: str = "output.txt") -> list[list]:
+    """Splits lines with "|" and returns nested list."""
     with open(filename) as file:
-        lines = file.read().splitlines()
-        [line.split("|").strip() for line in lines]
+        lines = [
+            [inner.strip() for inner in line]  # Second strip whitespaces
+            # First split each line in file.
+            for line in [line.split("|") for line in file.read().splitlines()]
+        ]
 
     return lines
 
 
 splitted_lines = split_filelines("output.txt")
+print("Splitted lines into lists.")
 
-print("\n\n")
-print()
+
+def padding_len(nested: list) -> int:
+    """Returns the length of the longest element in the table."""
+
+    # First make a list of the lengths of each separate table element.
+    lens: list[list[int]] = [[map(len, el) for el in nest] for nest in nested]
+    
+    # Make list of longest lengths in each nested list.
+    maxes: list[int] = [max(nest) for nest in lens]
+    
+    # Finally get longest length.
+    return [max(el) for el in maxes][0]
