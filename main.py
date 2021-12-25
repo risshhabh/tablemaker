92100f151bc6c81b380240a35f4dbf8e9a2b97ed
CodingSoda <26rkshah@gmail.com>
2021-12-24 19:36:27 -0800

    Created 'split_filelines()' in main.py

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 def init_files(input_name: str = "input.txt", output_name: str = "output.txt") -> list:
-    
+
     # Input
     try:
-        lines = open(f"{input_name}", "r").read().splitlines()
+        lines = open(input_name).read().splitlines()
     except FileNotFoundError:
         print("Looks like you don't have your files set up, I did it for you.")
-        open(f"{input_name}", "x")
-        lines = open(f"{input_name}", "r").read().splitlines()
+        open(input_name, "x")
+        lines = open(input_name).read().splitlines()
 
     # Output
     try:
-        open(f"{output_name}", "r")
+        open(output_name)
     except FileNotFoundError:
-        open(f"{output_name}", "x")
+        open(output_name, "x")
 
     return lines
 
 
-lines = init_files()
+lines = init_files("input.txt", "output.txt")
 
 
-def rm_cmnts(lines: list) -> int:
+def rm_cmnts(lines: list, output_name: str = "output.txt") -> int:
     lines = [line.strip() for line in lines]
+    err = 0
 
-    with open("output.txt", "w") as out:
+    with open(output_name, "w") as out:
         try:
             [out.write(line + "\n") if line[0] != "#" else ... for line in lines]
         except:
-            raise
+            err += 1
 
         out.close()
-        return 0
+        return err
 
 
-print(f"Removed Comments with {rm_cmnts(lines)}")
+print(f"Removed Comments with {rm_cmnts(lines, 'output.txt')}")
+
+
+def split_filelines(filename: str = "output.txt") -> list[list]:
+    with open(filename) as file:
+        lines = file.read().splitlines()
+        [line.split("|").strip() for line in lines]
+
+    return lines
+
+
+splitted_lines = split_filelines("output.txt")
+
+print("\n\n")
+print()
